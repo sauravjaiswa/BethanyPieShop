5c958300f4c7db48e2285766933a1171fca0dd8c
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-05-09 20:58:05 +0530

    Add project files.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese Cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId=1, CategoryName="Fruit Pies", Description="All-fruit"},
+                new Category{CategoryId=2, CategoryName="Cheese cakes", Description="Cheesy cakes"},
+                new Category{CategoryId=3, CategoryName="Seasonal Pies", Description="Get in seasons"}
+            };
+    }
+}
